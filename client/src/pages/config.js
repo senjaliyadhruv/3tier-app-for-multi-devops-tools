e27fd96a9d37efe6a0e137f57628a5564e408421
Dhruv Senjaliya <senjaliyadhruvr@gmail.com>
2025-07-30 17:24:57 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://backend.dhruvs.info";
+const API_BASE_URL = "http://backend-37499419.us-east-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
